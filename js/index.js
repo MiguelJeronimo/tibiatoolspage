bd2306167210eead21fd9e523f88ccee3a14724b
Miguel Jerónimo <33616302+MiguelJeronimo@users.noreply.github.com>
2023-01-31 12:42:59 -0600

    Agregando el active del menu con js

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ body.onload = ()=>{
     Home()
 }
 /*EVENTOS*/
-aviso.onclick = (e)=> {
+aviso.onclick = (e)=>{
     e.preventDefault()
     AvisoPrivacidad()
 }
@@ -31,12 +31,15 @@ caracteristicas.onclick = (e)=>{
  * Aparecer data en el aviso de privacidad
  */
 function AvisoPrivacidad() { 
+    aviso.classList.add('active')
+    home.classList.remove('active')
+    caracteristicas.classList.remove('active')
     aviso_data.classList.remove('ocultar');
-    aviso_data.classList.add('animate__animated','animate__fadeIn','animate__delay-2s')
+    aviso_data.classList.add('animate__animated','animate__fadeIn','animate__delay-1s')
     about_data.classList.add('ocultar')
-    about_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-2s')
+    about_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
     caracteristicas_data.classList.add('ocultar')
-    caracteristicas_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-2s')
+    caracteristicas_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
     titulo.classList.remove('animate__animated','animate__bounce')
  }
 
@@ -44,29 +47,37 @@ function AvisoPrivacidad() {
   * Aparece la data del home
   */
  function Home() {
+    aviso.classList.remove('active')
+    home.classList.add('active')
+    caracteristicas.classList.remove('active')
     about_data.classList.remove('ocultar')
-    about_data.classList.add('animate__animated','animate__fadeIn','animate__delay-2s')
+    about_data.classList.add('animate__animated','animate__fadeIn','animate__delay-1s')
     setTimeout(()=>{
     titulo.classList.add('animate__animated','animate__bounce')
     },3000)
     about_data.classList.add('animate__bounce','animate__delay-2s')
     aviso_data.classList.add('ocultar')
-    aviso_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-2s')
+    aviso_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
     caracteristicas_data.classList.add('ocultar')
-    caracteristicas_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-2s')
+    caracteristicas_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
  }
 
 /**
  * Aparece la data de las caracteristicas
  */
 function Caracteristicas() { 
+    aviso.classList.remove('active')
+    home.classList.remove('active')
+    caracteristicas.classList.add('active')
     caracteristicas_data.classList.remove('ocultar')
-    caracteristicas_data.classList.add('animate__animated','animate__fadeIn','animate__delay-2s')
+    caracteristicas_data.classList.add('animate__animated','animate__fadeIn','animate__delay-1s')
     about_data.classList.add('ocultar')
-    about_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-2s')
+    about_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
     aviso_data.classList.add('ocultar')
-    aviso_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-2s')
+    aviso_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
     titulo.classList.remove('animate__animated','animate__bounce')
  }
 
- 
+ /**
+  * Quitar el active del menu
+  */
