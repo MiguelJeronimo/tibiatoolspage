de98dd442ac888b05bfdf6f94e1cb0b756b0f1ab
Miguel Jerónimo <33616302+MiguelJeronimo@users.noreply.github.com>
2023-02-03 10:12:27 -0600

    Se modifico la politica de privacidad y se elimino el javascript de la politica

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
 /*Donde se pintara la data*/
-let aviso_data = document.getElementById("aviso-privacidad-data")
 let about_data = document.getElementById("about-data")
 let caracteristicas_data = document.getElementById("caracteristicas-data")
 let titulo = document.getElementById('titulo')
 let tituloContacto = document.getElementById('titulo-contacto')
 let contacto_data = document.getElementById('contacto-data')
 /*Donde se activaran los eventos*/
-let aviso = document.getElementById("aviso")
 let home = document.getElementById("home")
 let caracteristicas = document.getElementById("caracteristicas")
 let contacto = document.getElementById('contacto')
@@ -22,11 +20,6 @@ window.onload = ()=>{
 }
 
 /*EVENTOS*/
-aviso.onclick = (e)=>{
-    e.preventDefault()
-    AvisoPrivacidad()
-}
-
 home.onclick = (e)=>{
     e.preventDefault()
     Home()
@@ -45,28 +38,11 @@ contacto.onclick = (e)=>{
 /**
  * Aparecer data en el aviso de privacidad
  */
-function AvisoPrivacidad() { 
-    aviso.classList.add('active')
-    home.classList.remove('active')
-    caracteristicas.classList.remove('active')
-    contacto.classList.remove('active')
-    aviso_data.classList.remove('ocultar');
-    aviso_data.classList.add('animate__animated','animate__fadeIn','animate__delay-1s')
-    about_data.classList.add('ocultar')
-    about_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
-    caracteristicas_data.classList.add('ocultar')
-    caracteristicas_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
-    contacto_data.classList.add('ocultar')
-    contacto_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
-    titulo.classList.remove('animate__animated','animate__bounce')
-    tituloContacto.classList.remove('animate__animated','animate__bounce')
- }
 
  /**
   * Aparece la data del home
   */
  function Home() {
-    aviso.classList.remove('active')
     home.classList.add('active')
     caracteristicas.classList.remove('active')
     contacto.classList.remove('active')
@@ -76,8 +52,6 @@ function AvisoPrivacidad() {
     titulo.classList.add('animate__animated','animate__bounce')
     },3000)
     about_data.classList.add('animate__bounce','animate__delay-2s')
-    aviso_data.classList.add('ocultar')
-    aviso_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
     caracteristicas_data.classList.add('ocultar')
     caracteristicas_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
     contacto_data.classList.add('ocultar')
@@ -89,7 +63,6 @@ function AvisoPrivacidad() {
  * Aparece la data de las caracteristicas
  */
 function Caracteristicas() { 
-    aviso.classList.remove('active')
     home.classList.remove('active')
     caracteristicas.classList.add('active')
     contacto.classList.remove('active')
@@ -97,8 +70,6 @@ function Caracteristicas() {
     caracteristicas_data.classList.add('animate__animated','animate__fadeIn','animate__delay-1s')
     about_data.classList.add('ocultar')
     about_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
-    aviso_data.classList.add('ocultar')
-    aviso_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
     contacto_data.classList.add('ocultar')
     contacto_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
     titulo.classList.remove('animate__animated','animate__bounce')
@@ -110,9 +81,7 @@ function Caracteristicas() {
   */
  function Contacto() {
     contacto.classList.add('active')
-    aviso.classList.remove('active')
     home.classList.remove('active')
-    aviso.classList.remove('active')
     caracteristicas.classList.remove('active')
     contacto_data.classList.remove('ocultar')
     contacto_data.classList.add('animate__animated','animate__fadeIn','animate__delay-1s')
@@ -120,8 +89,6 @@ function Caracteristicas() {
     caracteristicas_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
     about_data.classList.add('ocultar')
     about_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
-    aviso_data.classList.add('ocultar')
-    aviso_data.classList.remove('animate__animated','animate__fadeIn','animate__delay-1s')
     titulo.classList.remove('animate__animated','animate__bounce')
     setTimeout(()=>{
         tituloContacto.classList.add('animate__animated','animate__bounce')
